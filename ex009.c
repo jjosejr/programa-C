fb7d26b2ecf62392aef9ab2d546d839fb90cf547
José Júnior <86629800+jjosejr@users.noreply.github.com>
2025-05-20 21:23:33 -0300

    atuzalizei

---
+++ ex009.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+
+    numero1 = 10;
+    numero2 = 5;
+
+    if (numero1 > número2){
+        printf("O número 1 e maior que o número 2\n");
+    }else { 
+        printf("O número 1 é menor que o número 2?\n");
+
+    }
+}
+    
